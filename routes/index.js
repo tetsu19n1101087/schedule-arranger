5c3d0151577ea80f20b82d7dd828a39c1afbe066
gizensha0331 <gizensha0331@gmail.com>
2020-10-08 04:48:00 +0900

    Heroku対応

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const router = express.Router();
+const Schedule = require('../models/schedule');
+const moment = require('moment-timezone');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  if (req.user) {
+    Schedule.findAll({
+      where: {
+        createdBy: req.user.id
+      },
+      order: [['"updatedAt"', 'DESC']]
+    }).then((schedules) => {
+      schedules.forEach((schedule) => {
+        schedule.formattedUpdatedAt = moment(schedule.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
+      });
+      res.render('index', {
+        user: req.user,
+        schedules
+      });
+    });
+  } else {
+    res.render('index', {user: req.user});
+  }
 });
 
 module.exports = router;
