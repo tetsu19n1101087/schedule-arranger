21c920b339b77c4af4a40e05e15d4bff0242d429
tetsu19n1101087 <gizensha0331@gmail.com>
2025-04-13 14:50:47 +0900

    立ち上げ処理を簡略化

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Express.js アプリケーションをサーバーレス関数としてエクスポート
-const app = require("../app")
-
-// Vercel のサーバーレス関数としてエクスポート
-module.exports = app
--- app.js
+++ app.js
@@ -121,4 +121,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
+
 module.exports = app;
